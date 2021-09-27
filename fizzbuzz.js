e74f927b9c9ca5318420b7a424f796bc281a0cf0
jakedanielturner <jakedanielturner@gmail.com>
2021-09-27 23:27:35 +0100

    finished comments, removed plan

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,5 @@
 // Fizzbuzz main project file
 
-/* Plan of action
-1. Create a variable to store the target number
-2. Define fizz, buzz and fizzbuzz
-3. Create for loop to run through the numbers until the target number, outputting to the console the result of each
-4. Profit
-*/
-
 const target = 100;
 // This is the number the programme will run to - change it!
 
@@ -14,53 +7,30 @@ const fizz = 3;
 const buzz = 5;
 // These have been predefined meaning the user can easily change them later, if they wanted to change the rules of the game.
 
-const playGame = target => {
-    for (i=0; i < (target+1); i++) {
-        if ((i % fizz) === 0 && (i % buzz) === 0) {
-            console.log("Fizzbuzz");
-        }
-        // This code block checks if the number is both a multiple of three and five
-    
-        else if ((i % fizz) === 0) {
-            console.log("Fizz");
-        }
-        // This code block checks if the number is just a multiple of three
-    
-        else if ((i % buzz) === 0) {
-            console.log("Buzz");
-        }
-        // This code block checks if the number is just a mutliple of five
-
-        else {
-            console.log(i);
-        }
-        // If all code blocks above have failed, then the number is not a multiple of either and is outputted to the console as is.
-    }
-}
-
-/* Refactoring plan:
-1. Create seperate function to return a number or word based on the current value
-2. Output such value to the console
-*/
-
 const fizzOrBuzz = num => {
     if ((num % fizz) === 0 && (num % buzz) === 0) {
         return ("fizzbuzz");
     }
+    // This block of code checks if the given number is a multiple of both fizz and buzz
 
     else if ((num % fizz) === 0) {
         return ("fizz");
     }
+    // This block of code checks if the given number is a multiple of fizz
 
     else if ((num % buzz) === 0) {
         return ("buzz");
     }
+    // This block of code checks if the given number is a multiple of buzz
 
     else {
         return i;
     }
+    // If the number is neither a multiple of fizz or buzz, it is returned by itself
 }
 
 for (i=1; i<(target+1); i++) {
     console.log(fizzOrBuzz(i));
 }
+// Finally, the list of numbers is cycled through and the result of the function check is logged to the console, producing our list
+
