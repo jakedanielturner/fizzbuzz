a527c9d59e367260761f0858a220b7a3ce92a0aa
jakedanielturner <jakedanielturner@gmail.com>
2021-09-27 20:22:33 +0100

    created consts, playGame func

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,3 +7,25 @@
 4. Profit
 */
 
+const target = 0;
+// This is the number the programme will run to - change it!
+
+const fizz = 3;
+const buzz = 5;
+// These have been predefined meaning the user can easily change them later, if they wanted to change the rules of the game.
+
+const playGame = (target) => {
+    for (i=0; i++; i < (target+1)) {
+        if ((i % fizz) && (i % buzz)) {
+            console.log("Fizzbuzz");
+        }
+    
+        if (i % fizz) {
+            console.log("Fizz");
+        }
+    
+        else if (i % buzz) {
+            console.log("Buzz");
+        }
+    }
+}
