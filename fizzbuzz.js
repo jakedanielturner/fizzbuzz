3d2b91218e24be5513155f07a9e7a0dd79c85877
jakedanielturner <jakedanielturner@gmail.com>
2021-09-27 20:05:49 +0100

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+// Fizzbuzz main project file
+
+/* Plan of action
+1. Create a variable to store the target number
+2. Define fizz, buzz and fizzbuzz
+3. Create for loop to run through the numbers until the target number, outputting to the console the result of each
+4. Profit
+*/
+
